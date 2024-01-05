4516f34ea0cc33d8028144ca0f9459218f1a440d
JuliaOlowniuk <julia.olowniuk@gmail.com>
2024-01-05 15:05:57 +0100

    skalowanie2

--- sort.py
+++ sort.py
@@ -2,33 +2,45 @@ import tkinter as tk
 from tkinter import simpledialog
 from datetime import datetime
 
-
 def sort_tasks(conn, task_listbox):
     # Okno dialogowe do wyboru kryterium sortowania
-    result = simpledialog.askstring("Sortowanie", "Wybierz kryterium sortowania:\n1. Priorytet\n2. Data wykonania")
+    result = simpledialog.askstring("Sortowanie", "Wybierz kryterium sortowania:\n1. Priorytet rosnąco\n2. Priorytet malejąco\n3. Data wykonania rosnąco\n4. Data wykonania malejąco")
 
     if result:
         if result == "1":
-            sort_by_priority(conn, task_listbox)
+            sort_by_priority_asc(conn, task_listbox)
         elif result == "2":
-            sort_by_due_date(conn, task_listbox)
+            sort_by_priority_desc(conn, task_listbox)
+        elif result == "3":
+            sort_by_due_date_asc(conn, task_listbox)
+        elif result == "4":
+            sort_by_due_date_desc(conn, task_listbox)
         else:
-            tk.messagebox.showwarning("Uwaga", "Nieprawidłowy wybór. Wybierz 1 lub 2.")
+            tk.messagebox.showwarning("Uwaga", "Nieprawidłowy wybór. Wybierz liczbę od 1 do 4.")
 
-
-def sort_by_priority(conn, task_listbox):
+def sort_by_priority_asc(conn, task_listbox):
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM tasks ORDER BY priority ASC')
     tasks = cursor.fetchall()
     display_sorted_tasks(task_listbox, tasks)
 
+def sort_by_priority_desc(conn, task_listbox):
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tasks ORDER BY priority DESC')
+    tasks = cursor.fetchall()
+    display_sorted_tasks(task_listbox, tasks)
 
-def sort_by_due_date(conn, task_listbox):
+def sort_by_due_date_asc(conn, task_listbox):
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM tasks WHERE due_date IS NOT NULL ORDER BY datetime(due_date) ASC')
     tasks = cursor.fetchall()
     display_sorted_tasks(task_listbox, tasks)
 
+def sort_by_due_date_desc(conn, task_listbox):
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tasks WHERE due_date IS NOT NULL ORDER BY datetime(due_date) DESC')
+    tasks = cursor.fetchall()
+    display_sorted_tasks(task_listbox, tasks)
 
 def display_sorted_tasks(task_listbox, tasks):
     task_listbox.delete(0, tk.END)
@@ -36,12 +48,9 @@ def display_sorted_tasks(task_listbox, tasks):
         task_text = f"[{'x' if task[2] else ' '}] {task[1]} - Priorytet: {task[3]} - Data wykonania: {format_due_date(task[4])}"
         task_listbox.insert(tk.END, (task[0], task_text))
 
-
 def format_due_date(due_date):
     try:
-
         parsed_date = datetime.strptime(due_date, "%Y-%m-%d")
         return parsed_date.strftime("%d-%m-%Y")
     except ValueError:
-
         return due_date
