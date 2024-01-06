69ebb0442b9733196c0fed8cdb40c7cf1115c575
JuliaOlowniuk <julia.olowniuk@gmail.com>
2024-01-06 11:57:10 +0100

    proba nap edit

--- edit.py
+++ edit.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import simpledialog
 import sqlite3
 from load import load_tasks
+
 def edit_description(conn, task_listbox):
     selected_task_indices = task_listbox.curselection()
 
@@ -10,23 +11,24 @@ def edit_description(conn, task_listbox):
         selected_task_index = selected_task_indices[0]
         task_id = task_listbox.get(selected_task_index)[0]
 
-        current_description = get_task_description(conn, task_id)
-        new_description = simpledialog.askstring("Edytuj opis", "Wprowadź nowy opis zadania:", initialvalue=current_description)
+        current_note = get_task_note(conn, task_id)
+        new_note = simpledialog.askstring("Dodaj notatkę", "Wprowadź nową notatkę do zadania:", initialvalue=current_note)
 
-        if new_description is not None:
-            update_task_description(conn, task_id, new_description)
+        if new_note is not None:
+            update_task_note(conn, task_id, new_note)
             refresh_task_listbox(conn, task_listbox)
 
-def get_task_description(conn, task_id):
+def get_task_note(conn, task_id):
     cursor = conn.cursor()
-    cursor.execute('SELECT description FROM tasks WHERE id=?', (task_id,))
+    cursor.execute('SELECT note FROM tasks WHERE id=?', (task_id,))
     result = cursor.fetchone()
     return result[0] if result else ""
 
-def update_task_description(conn, task_id, new_description):
+def update_task_note(conn, task_id, new_note):
     cursor = conn.cursor()
-    cursor.execute('UPDATE tasks SET description=? WHERE id=?', (new_description, task_id))
+    cursor.execute('UPDATE tasks SET note=? WHERE id=?', (new_note, task_id))
     conn.commit()
 
 def refresh_task_listbox(conn, task_listbox):
-    load_tasks(conn, task_listbox)
+    # Nie aktualizuj listy tutaj, zamiast tego zaktualizuj po ponownym kliknięciu na zadanie
+    pass
